c83b9cf5b6ba1e8a81fdccc52ddf01f67a31f830
kartikkpawar <knpwar789@gmail.com>
2024-11-19 10:48:12 +0530

    feat: style

--- client/app/dashboard/account/page.jsx
+++ client/app/dashboard/account/page.jsx
@@ -8,6 +8,7 @@ import { getPasswords } from "@/actions/password";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { copyPassword } from "@/actions/password";
+import { CircleSlash2Icon } from "lucide-react";
 
 function UserAccoutsPage() {
   const query = useQuery({
@@ -37,18 +38,24 @@ function UserAccoutsPage() {
     },
   });
 
-  const copyEncText = (text) => {
-    console.log(text, "copyEncText");
-    const decryptText = mutation.mutate(text);
-    console.log(decryptText, "decryptText");
-  };
-
   return (
     <div className="w-full flex flex-col gap-5 h-full">
       <Searchbar name="Accounts" button={AddAccPassBtn} />
+      {(!query?.data?.data || query?.data?.data?.length === 0) && (
+        <div className="flex h-full w-full items-center justify-center flex-col gap-3">
+          <CircleSlash2Icon size={140} className="text-muted-foreground" />
+          <span>No data available</span>
+        </div>
+      )}
       <div className="h-full grid grid-cols-1 md:grid-cols-3 gap-5">
         {query?.data?.data?.map((data) => (
-          <AccPassCard data={data} key={data._id} onCopy={copyEncText} />
+          <AccPassCard
+            data={data}
+            key={data._id}
+            onCopy={() => {
+              mutation.mutate(text);
+            }}
+          />
         ))}
       </div>
     </div>
